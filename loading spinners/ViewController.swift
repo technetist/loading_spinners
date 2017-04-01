4475ee793881736f766fa61394d8a5a4fb11dd85
Adrien Maranville <adrien.maranville@gmail.com>
2017-04-01 11:14:11 -0600

    Initial Commit

---
+++ loading spinners/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  loading spinners
+//
+//  Created by Adrien Maranville on 4/1/17.
+//  Copyright © 2017 Adrien Maranville. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
