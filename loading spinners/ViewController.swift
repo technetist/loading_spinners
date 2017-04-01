a114aaacf988f883be65b70d9ca43a43e394d61c
Adrien Maranville <adrien.maranville@gmail.com>
2017-04-01 12:01:41 -0600

    created an app with alerts and loading spinners

--- loading spinners/ViewController.swift
+++ loading spinners/ViewController.swift
@@ -9,7 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var activityIndicator = UIActivityIndicatorView()
 
+    @IBAction func btnTriggerAlert(_ sender: Any) {
+        let alertController = UIAlertController(title: "Howdy Stranger", message: "Are you awesome?", preferredStyle: UIAlertControllerStyle.alert)
+        
+        alertController.addAction(UIAlertAction(title: "Yes", style: .default, handler: { (action) in
+            print("button pressed")
+            self.dismiss(animated: true, completion: nil)
+        }))
+        
+        alertController.addAction(UIAlertAction(title: "No", style: .default, handler: { (action) in
+            print("button pressed")
+            self.dismiss(animated: true, completion: nil)
+        }))
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    @IBAction func btnPauseApp(_ sender: Any) {
+        
+        activityIndicator = UIActivityIndicatorView (frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        
+        activityIndicator.center = self.view.center
+        
+        activityIndicator.hidesWhenStopped = true
+        
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        
+        view.addSubview(activityIndicator)
+        
+        activityIndicator.startAnimating()
+        
+        //UIApplication.shared.beginIgnoringInteractionEvents()
+        
+    }
+    @IBAction func btnRestartApp(_ sender: Any) {
+        activityIndicator.stopAnimating()
+        
+        //UIApplication.shared.endIgnoringInteractionEvents()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
